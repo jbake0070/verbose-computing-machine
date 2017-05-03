ad35d06f2db3d52797086103296835d9b24e9f79
jbake0070 <baker.james002@gmail.com>
2017-05-03 12:41:46 -0600

    Yo fam

---
+++ src/AirplaneData.java
@@ -0,0 +1,14 @@
+
+public class AirplaneData {
+
+	public void setNumberEngines(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getNumberEngines() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/Flyer.java
@@ -0,0 +1,9 @@
+
+public class Flyer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
